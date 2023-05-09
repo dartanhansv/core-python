a46262b1a78e8a5c220a342e221e7cfc722a4665
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-05-09 16:05:37 -0300

    add ordinal-suffix function

---
+++ ord-suffix.py
@@ -0,0 +1,18 @@
+
+def nth_root(radicand, n):
+    return radicand ** (1/n)
+
+def ordinal_suffix(value):
+    s = str(value)
+    if s.endswith('11'):
+        return 'th'
+    elif s.endswith('12'):
+        return 'th'
+    elif s.endswith('13'):
+        return 'th'
+    elif s.endswith('1'):
+        return 'st'
+    elif s.endswith('2'):
+        return 'nd'
+    elif s.endswith('3'):
+        return 'th'
