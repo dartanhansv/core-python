f3ea22fe0945f51cc24992c3171054bbab554559
Dartanhan S. Venturini <dartanhan@outlook.com>
2023-05-09 16:13:27 -0300

    add ordinal function

--- ord-suffix.py
+++ ord-suffix.py
@@ -20,3 +20,10 @@ def ordinal_suffix(value):
 
 def ordinal(value):
     return str(value) + ordinal_suffix(value)
+
+
+def display_nth_root(radicand, n):
+    root = nth_root(radicand, n)
+    message = "The" + ordinal(n) + "root of " + str(radicand) + "is" + str(root)
+
+    print(message)
